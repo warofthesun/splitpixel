ece270f954a083d507ca7b3f48255343a169b47c
Dan Brown <fireflip@gmail.com>
2017-07-30 14:40:39 -0500

    Testing Atom

--- header-contact.php
+++ header-contact.php
@@ -41,20 +41,13 @@
 	</head>
 
 	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
-
 		<div id="container">
-
 			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
-
 				<div id="inner-header" class="wrap cf">
-
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
 					<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="<?php echo home_url(); ?>" rel="nofollow"><?php bloginfo('name'); ?></a></p>
-
 					<?php // if you'd like to use the site description you can un-comment it below ?>
 					<?php // bloginfo('description'); ?>
-
-
 					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
 						<?php wp_nav_menu(array(
     					         'container' => false,                           // remove nav container
@@ -71,7 +64,6 @@
 						)); ?>
 
 					</nav>
-
 				</div>
 				<div class="intro">let's<div class="largetext">talk</div>about you!</div>
 			</header>
