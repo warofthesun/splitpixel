842ecac0b2ee7be119a333ad2263efb46f2c56ae
Daniel Brown <danielbrown@Daniels-MBP.attlocal.net>
2018-01-01 12:07:10 -0600

    Padding tweaks

--- footer.php
+++ footer.php
@@ -18,7 +18,7 @@
 						)); ?>
 					</nav>
 
-					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?> | <a href="mailto:fireflip@gmail.com">fireflip@gmail.com</a></p>
+					<p class="source-org copyright">&copy; <?php echo date('Y'); ?> <?php bloginfo( 'name' ); ?> | <a href="mailto:daniel@thesplitpixel.com">daniel@thesplitpixel.com</a></p>
 
 				</div>
 
