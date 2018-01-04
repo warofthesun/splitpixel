522f7b6722ffc0c94c0b84a711fec40f76896f9a
Daniel Brown <danielbrown@Daniels-MacBook-Pro.local>
2018-01-03 21:18:54 -0600

    Full width front page hero image

--- front-page.php
+++ front-page.php
@@ -6,23 +6,27 @@
 ?>
 <?php get_header(); ?>
 <!--FRONT-PAGE.PHP-->
+		<div class="header_wrap">
+			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<div class="welcome wrap cf">
+				<div class="d-3of7 t-1of2 m-1of3">
+					&nbsp;
+				</div>
+				<div class="d-4of7 t-1of2 m-2of3 last-col section_one">
+					<div><?php the_field('section_one'); ?></div>
+				</div>
+			</div>
+		</div>
 			<div id="content" class="front-page">
 
 				<div id="inner-content" class="wrap cf">
 
 						<main id="main" class="m-all t-3of3 d-7of7 cf" role="main" itemscope itemprop="mainContentOfPage" itemtype="http://schema.org/Blog">
 
-							<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+
 
 							<article id="post-<?php the_ID(); ?>" <?php post_class( 'cf' ); ?> role="article">
-								<div class="welcome">
-									<div class="d-3of7 t-1of2 m-1of3">
-										&nbsp;
-									</div>
-									<div class="d-4of7 t-1of2 m-2of3 last-col section_one">
-										<div><?php the_field('section_one'); ?></div>
-									</div>
-								</div>
+
 								<div style="clear:both"></div>
 								<div class="about">
 									<div class="d-2of3 t-all m-all">
